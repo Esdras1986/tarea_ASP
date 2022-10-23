cc239d3c7c37aeeb1cd6ba438f7fea4fcd99de72
Esdras1986 <104287914+Esdras1986@users.noreply.github.com>
2022-10-23 12:58:23 -0600

    Add files via upload

--- Modelo/ConexionBD.cs
+++ Modelo/ConexionBD.cs
@@ -12,21 +12,43 @@ namespace Modelo
 {
     public class ConexionBD {
         private string cadena = "server=localhost; database=db_empresa; user=root; password=administrador ";
-        public MySqlConnection conectar;
+        public MySqlConnection conectar= new MySqlConnection();
         public void Abrirconexion(){
             try 
             {
 
-            conectar = new MySqlConnection();
+            
             conectar.ConnectionString = cadena;
             conectar.Open();
-                System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+            //System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
             }
             catch (Exception ex){
 
                 System.Diagnostics.Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
 
             }
         }
+        public void CerrarConexion()
+        {
+            try
+            {
+                if(conectar.State == ConnectionState.Open) 
+                {
+                    conectar.Clone();
+                   // System.Diagnostics.Debug.WriteLine("Cerrar conexion");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+        }
+
+
     }
+
 }
--- Modelo/Empleado.cs
+++ Modelo/Empleado.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web.UI.WebControls;
 
 namespace Modelo
 {
     public class Empleado
     {
+        ConexionBD conectar;
+        private DataTable drop_puesto() 
+        {
+            DataTable tabla = new DataTable();
+            conectar = new ConexionBD();
+            string consulta = String.Format("selec id_puesto as id, puesto from puestos");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            query.Fill(tabla);
+            conectar.CerrarConexion();
+            return tabla;
+        }
+        private DataTable grid_empleados()
+        {
+            DataTable tabla = new DataTable();
+            conectar = new ConexionBD();
+            string consulta = String.Format("selec id_puesto as id, puesto from puestos");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            query.Fill(tabla);
+            conectar.CerrarConexion();
+            return tabla;
+        }
     }
 }
